7a5cfec9c345bd15d7e648e10ff02448a8c7a238
ChrlnSaez <charlenesaez03@gmail.com>
2022-07-26 12:14:01 +0800

    image refactor

--- src/components/ModelCard.js
+++ src/components/ModelCard.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ModelCard = ({
   name,
@@ -12,37 +12,41 @@ const ModelCard = ({
   isLanding,
 }) => {
   return (
-    <div className="py-8 rounded-lg bg-white text-gray-900 px-16">
+    <div className='py-8 rounded-lg bg-white text-gray-900 px-16'>
       <div
         className={`flex flex-col gap-4 justify-center items-center ${
-          isLanding ? "flex-col-reverse" : ""
+          isLanding ? 'flex-col-reverse' : ''
         }`}>
-        <h3 className="font-semibold text-xl text-center">{name}</h3>
-        <img src={image} alt="car" className="w-64 h-40" />
+        <h3 className='font-semibold text-xl text-center'>{name}</h3>
+        <img
+          src={image}
+          alt='car'
+          className={`w-64 h-40 ${isLanding && 'w-72 h-48'}`}
+        />
         {!isLanding && (
           <>
-            <div className="flex items-center justify-between w-full">
-              <h4 className="text-lg font-medium">{type}</h4>
-              <p className="bg-green-600 text-white text-sm py-1 px-2 font-semibold rounded-full">
-                Php {rate.toLocaleString("en-US")}.00/day
+            <div className='flex items-center justify-between w-full'>
+              <h4 className='text-lg font-medium'>{type}</h4>
+              <p className='bg-green-600 text-white text-sm py-1 px-2 font-semibold rounded-full'>
+                Php {rate.toLocaleString('en-US')}.00/day
               </p>
             </div>
-            <div className="w-full h-px bg-gray-200" />
-            <div className="flex justify-center text-center">
-              <p className="w-64 h-24">{description}</p>
+            <div className='w-full h-px bg-gray-200' />
+            <div className='flex justify-center text-center'>
+              <p className='w-64 h-24'>{description}</p>
             </div>
           </>
         )}
-        <div className="flex w-full flex-col gap-2 text-white">
+        <div className='flex w-full flex-col gap-2 text-white'>
           {!isUse && (
             <>
               <Link to={`/rent/${_id}`}>
-                <button className="py-1 w-full px-4 rounded-full bg-yellow-600 hover:bg-yellow-500">
+                <button className='py-1 w-full px-4 rounded-full bg-yellow-600 hover:bg-yellow-500'>
                   Rent
                 </button>
               </Link>
               <Link to={`/reserve/${_id}`}>
-                <button className="py-1 w-full px-4 rounded-full bg-yellow-600 hover:bg-yellow-500">
+                <button className='py-1 w-full px-4 rounded-full bg-yellow-600 hover:bg-yellow-500'>
                   Reserve
                 </button>
               </Link>
